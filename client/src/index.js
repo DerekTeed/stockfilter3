2c43435439485d918a6ee43d0c216569756f7ba7
thaining7 <thaining@protonmail.com>
2019-06-24 11:41:47 -0500

    fixed sequlize string operator error by manually updating sequelize to version 5.8.12, changed server PORT to 3000 and SQL PORT to 3306, change manually according to your local machine

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,21 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import api from "eodhistoricaldata-api";
+import "./styles.css";
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // // If you want your app to work offline and load faster, you can change
 // // unregister() to register() below. Note this comes with some pitfalls.
 // // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+serviceWorker.unregister();
 
-import React from "react";
-import ReactDOM from "react-dom";
-import api from "eodhistoricaldata-api";
-import "./styles.css";
+// import React from "react";
+// import ReactDOM from "react-dom";
+
 
 class Eodhistoricaldata extends React.Component {
   state = { data: [] };
