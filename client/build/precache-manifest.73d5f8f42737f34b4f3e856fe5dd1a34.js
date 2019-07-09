181ebd8d5a2e68ffe540624352d8d76caf05f997
Derek Teed <Derek@MacBook-Air.local>
2019-07-09 07:35:02 -0500

    build added

---
+++ client/build/precache-manifest.73d5f8f42737f34b4f3e856fe5dd1a34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aad05703ebbc70c9f67ae13cbd6c94b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2ab22ce927630609277",
+    "url": "/static/css/main.a893775f.chunk.css"
+  },
+  {
+    "revision": "9f2ca83cb2f7db49a700",
+    "url": "/static/js/2.94d2338a.chunk.js"
+  },
+  {
+    "revision": "c2ab22ce927630609277",
+    "url": "/static/js/main.b1242c0c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
